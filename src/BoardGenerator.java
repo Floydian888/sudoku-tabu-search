c0285d14fc061d88f0c540b5e86b05ea88e9534a
unknown <mdaniluk123@gmail.com>
2014-12-05 10:04:37 +0100

    Implemented BoardGenerator functions: generateCorrectBoard, isCorrect Add SudokuCell class

--- src/BoardGenerator.java
+++ src/BoardGenerator.java
@@ -1,22 +1,87 @@
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class BoardGenerator {
 
-	private int [][] board;
-	
-	public boolean isCorrect(){
-		return false;
+	private SudokuCell [][] board;
+
+	public boolean isCorrect(int size){
+		return Helpers.isCorrect(board, size);
 	}
 	
+	private boolean isSafe(int num, int row, int col, int size) {
+		return Helpers.isSafe(board, num, row, col, size);
+	}
+	
+	private void clearCell(int cell, int size) {
+		int col = cell % size;
+		int row = cell / size;
+		board[row][col].clear();
+	}
+	
+	private boolean generateCorrectBoard(int size, int currentCell, ArrayList<Integer> availableNumbers) {
+		if (currentCell == size*size )
+			return true;
+		
+		int col = currentCell % size;
+		int row = currentCell / size;
+
+		if (availableNumbers.size() > 0) {
+			Collections.shuffle(availableNumbers);
+			int num = availableNumbers.get(0);
+			if (isSafe(num, row, col, size)) {
+				board[row][col].set(num);
+				availableNumbers.clear();
+				for (int i = 1; i <= size; i++)
+					availableNumbers.add(i);
+				if (generateCorrectBoard(size, currentCell + 1, availableNumbers) )
+					return true;
+			} 
+			else{
+				availableNumbers.remove(new Integer(num));
+				if(generateCorrectBoard(size, currentCell, availableNumbers))
+					return true;
+			}
+		}
+		else {
+			for (int i = 1; i <= size; i++)
+				availableNumbers.add(i);
+			
+			clearCell(currentCell - 1, size);
+			clearCell(currentCell - 2, size);
+			clearCell(currentCell - 3, size);
+			clearCell(currentCell - 4, size);
+			clearCell(currentCell - 5, size);
+			clearCell(currentCell - 6, size);
+			clearCell(currentCell - 7, size);
+			clearCell(currentCell - 8, size);
+			clearCell(currentCell - 9, size);
+			if(generateCorrectBoard(size, currentCell - 9, availableNumbers))
+				return true;
+		}
+		return false;
+	}
 	public void generateCorrectBoard(int size){
 		
+		board = new SudokuCell[size][size];
+		for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+            	board[i][j] = new SudokuCell();
+            }
+        }
+		ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+		for (int i = 1; i <= 9; i++)
+			availableNumbers.add(i);
+		generateCorrectBoard(size, 0, availableNumbers);	
+
 	}
 	
 	public void removeNumbers(int leftNumbersNmb){
 		
 	}
 	
-	public int [][] getCurrentBoard(){
-		return null;
+	public SudokuCell [][] getCurrentBoard(){
+		return board;
 	}
 	
 }
--- src/Helpers.java
+++ src/Helpers.java
@@ -1,8 +1,90 @@
+import java.util.ArrayList;
+
 
 public class Helpers {
 
-	public static boolean isCorrect(int [][] board){
+	public static boolean isCorrect(SudokuCell [][] board, int size){
+		for (int i = 0; i < size; i++)
+			if (!isCorrectCol(board, i, size) || !isCorrectRow(board, i, size))
+				return false;
+		for (int row = 0; row < size; row += 3)
+			for (int col = 0; col < size; col += 3)
+				if (!isCorrectBox(board, row, col, size))
+					return false;
+		
 		return true;
 	}
+	public static boolean isSafe(SudokuCell [][] board, int num, int row, int col, int size) {
+		return (!usedInCol(board, num, col, size) && !usedInRow(board, num, row, size) && !usedInBox(board, num, row - row%3, col - col%3, size));
+	}
 	
+	private static boolean isCorrectCol(SudokuCell [][] board, int col, int size) {
+		ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+		for (int i = 1; i <= size; i++)
+			availableNumbers.add(i);
+		
+		for (int row = 0; row < size; row++)
+			availableNumbers.remove(new Integer(board[row][col].get()));
+		
+		if (availableNumbers.size() == 0)
+			return true;
+		
+		return false;
+			
+	}
+	
+	private static boolean isCorrectRow(SudokuCell [][] board, int row, int size) {
+		ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+		for (int i = 1; i <= size; i++)
+			availableNumbers.add(i);
+		
+		for (int col = 0; col < size; col++)
+			availableNumbers.remove(new Integer(board[row][col].get()));
+		
+		if (availableNumbers.size() == 0)
+			return true;
+		
+		return false;
+			
+	}
+	
+	private static boolean isCorrectBox(SudokuCell [][] board, int boxStartRow, int boxStartCol, int size) {
+		ArrayList<Integer> availableNumbers = new ArrayList<Integer>();
+		for (int i = 1; i <= size; i++)
+			availableNumbers.add(i);
+		
+		for (int row = 0; row < 3; row++)
+			for (int col = 0; col < 3; col++)
+				availableNumbers.remove(new Integer(board[row + boxStartRow][col + boxStartCol].get()));
+		
+		if (availableNumbers.size() == 0)
+			return true;
+		
+		return false;
+	}
+	
+	private static boolean usedInCol(SudokuCell [][] board, int num, int col, int size) {
+		for (int row = 0; row < size; row++)
+			if (board[row][col].get() == num)
+				return true;
+
+		return false;
+	}
+	
+	private static boolean usedInRow(SudokuCell [][] board, int num, int row, int size) {
+		for (int col = 0; col < size; col++)
+			if (board[row][col].get() == num)
+				return true;
+
+		return false;
+	}
+	
+	private static boolean usedInBox(SudokuCell [][] board, int num, int boxStartRow, int boxStartCol, int size) {
+		for (int row = 0; row < 3; row++)
+			for (int col = 0; col < 3; col++)
+				if (board[row + boxStartRow][col + boxStartCol].get() == num)
+					return true;
+		
+		return false;
+	}
 }
